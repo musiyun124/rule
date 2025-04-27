e577cec87a8076e43ad8ec1eae1281fb8a554d56
MoGuangYu <staryuboy@gmail.com>
2025-04-28 07:48:30 +0800

    2025-04-28 07:48

--- script/convert.sh
+++ script/convert.sh
@@ -47,8 +47,12 @@ find . -name "*_OCD_*.txt" | while read -r file; do
     if [[ "$first_line" == *"payload"* ]]; then
         sed -i '1d' "$file"
     fi
-    # 删除所有单引号、空格
-    sed -i "s/'//g; s/[[:space:]]//g; s/[^a-zA-Z0-9.-]//g" "$file"
+    
+    # 删除所有单引号和空格，保留域名中的"-"
+    sed -i "s/'//g; s/[[:space:]]//g" "$file"
+
+    # 处理保留 "-" 但不删除
+    sed -i 's/[^.]*-[^.]*/\1-\2/g' "$file"
 
     file_dir=$(dirname "$file")
     filename=$(basename "$file" .txt)
